a4a6dde48b467f250a32d29234097c1446d41f95
iTsRH98 <rhnayemfardin@gmail.com>
2025-04-18 01:10:43 +0600

    added book details and ratings

--- src/pages/Books/Book.jsx
+++ src/pages/Books/Book.jsx
@@ -1,26 +1,27 @@
 import React, { use } from "react";
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const Book = ({ book }) => {
-  const { bookName, author, image } = book;
+  const { bookName, author, image, review, rating } = book;
   // const bookData=use(booksPromise)
   // console.log(bookData);
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
-      <figure className="p-4">
-        <img src={image} alt="Shoes" />
+    <div className="card bg-base-100 w-96 shadow-sm border border-gray-300 my-12">
+      <figure className="p-4 bg-gray-100 w-2/3 mx-auto m-4">
+        <img className="h-[166px]" src={image} alt="" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
-          Card Title
+          {bookName}
           <div className="badge badge-secondary">NEW</div>
         </h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
+        <p>{review}</p>
         <div className="card-actions justify-end">
           <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+          <div className="badge badge-outline">
+            {rating}
+            <FaStarHalfAlt />
+          </div>
         </div>
       </div>
     </div>
